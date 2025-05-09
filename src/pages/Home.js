1c2eac4a0ba0d5b1d6381bee31cfd5c6673f9a5a
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-09 10:19:56 +0300

    fetching data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,43 @@
 import supabase from "../config/supabaseClient"
+import {useEffect, useState} from 'react'
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null)
+  const [users,setUsers] = useState(null)
   
+  useEffect(()=>{
+    const fetchUsers = async () => {
+      const {data,error} = await supabase
+        .from('users')
+        .select()
+      if(error){
+        setFetchError('could not fetch users')
+        console.log(error);
+        setUsers(null)
+      }
+
+      if (data) {
+        setUsers(data)
+        setFetchError(null)
+      }
+    }
+    fetchUsers()
+    }
+  ,[])
+  if(users){
+    console.log(users)
+  }
   return (
     <div className="page home">
       <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {users && (
+        <div>
+          {users.map(user => (
+            <p key={user.id}>{user.age}</p>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
